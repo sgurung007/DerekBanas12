70ced432cd022dac4c43d41d9471088ee7dc8619
suraj gurung <sgrg007@gmail.com>
2020-12-05 10:23:10 -0600

    Add project files.

---
+++ DerekBanas12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DerekBanas12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
